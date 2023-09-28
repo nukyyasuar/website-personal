e46bbf16a55a6872afcf378a68c9dcd29dd51945
nukyyasuar <nukyyasuarzamzamy@gmail.com>
2023-09-28 10:33:07 +0700

    First Commit

---
+++ Personal_Web/home.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    Particles.init({
+      selector: '.background',
+      maxParticles: 150,
+      connectParticles: 'true',
+      speed:0.5,
+      minDistance: 150,
+      sizeVariations: 10,
+      color: '#7D7C7C'
+      
+    });
+}
